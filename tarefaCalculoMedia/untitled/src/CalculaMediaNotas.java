f207953c0fc83533e5aac0ed9280f22c9d40df4f
rf <71889267+ramonfelizetti@users.noreply.github.com>
2023-01-19 19:48:35 -0300

    Tarefa modulo 08

---
+++ tarefaCalculoMedia/untitled/src/CalculaMediaNotas.java
@@ -0,0 +1,11 @@
+public class CalculaMediaNotas {
+    public void calcular() {
+        int notaUm = 3;
+        int notaDois = 7;
+        int notaTres = 5;
+        int notaQuatro = 5;
+
+        int media = (notaUm +  notaDois + notaTres + notaQuatro) / 4;
+        System.out.println("Média Final: " + media);
+    }
+}
---
+++ tarefaCalculoMedia/untitled/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        CalculaMediaNotas calculaMediaNotas = new CalculaMediaNotas();
+        calculaMediaNotas.calcular();
+    }
+}
